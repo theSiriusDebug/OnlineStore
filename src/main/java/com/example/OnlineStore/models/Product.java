a23dcdf56d3023a53d98b67f522eaebb5db1d11b
Sirius M <mihail.chernoby@gmail.com>
2023-05-10 09:43:17 +0300

    commit

--- src/main/java/com/example/OnlineStore/models/Product.java
+++ src/main/java/com/example/OnlineStore/models/Product.java
@@ -19,6 +19,7 @@ public class Product {
     private String description;
     private int rate;
     private int quantity;
+    private long price;
 
     @JsonIgnore
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
